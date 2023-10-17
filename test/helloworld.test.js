7bceb99869ab8fa1458d84fe9e3ba603e373b937
jmracines3 <jmracines3@gmail.com>
2023-10-18 00:16:44 +0800

    first commit

---
+++ test/helloworld.test.js
@@ -0,0 +1,11 @@
+import sayHello from "../Week 6/helloworld.mjs";
+import assert from 'assert';
+
+it("Tests Hello World"), ()=>{
+
+    const hello = sayHello();
+
+    assert.equal(hello,"hello");
+
+
+}
